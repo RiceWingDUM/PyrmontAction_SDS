76f2789be04ad7def3ae67b146c65419bb3e10a6
Anthony He <114205788+RiceWingDUM@users.noreply.github.com>
2025-10-08 19:20:05 +1100

    added note field for MeetingMinute document in database

--- backend/apps/models/meetingMinuteModel.js
+++ backend/apps/models/meetingMinuteModel.js
@@ -4,6 +4,7 @@ const meetingMinuteSchema = new mongoose.Schema({
 
         title: { type: String, required: true },
         meetingDate: { type: Date, required: true },
+        note: {type: String },
         fileUrl: { type: String, required: true },
         status: { type: String, enum: ['draft', 'published'], default: 'draft' },
 
