365c89acc55b3137550895081699ae8da39275fa
Henil <henilp467@gmail.com>
2025-10-09 13:29:18 +1100

    [e2e/contactForm.spec.js]: e2e test to verify correct validation of empty contact form fields submission

--- tests/e2e/contactForm.spec.js
+++ tests/e2e/contactForm.spec.js
@@ -8,4 +8,11 @@ test.describe("End-to-End tests for Contact Form", () => {
         await expect(page.locator("#firstName")).toBeVisible();
         await expect(page.locator("#email")).toBeVisible();
     });
+
+    // Validate submission attempt with empty contact form fields to show errors
+    test("displays validation errors when submitting empty form", async ({ page }) => {
+        await page.goto("http://localhost:5173/contact");
+        await page.click(".submit-btn");
+        await expect(page.locator(".error-message")).toBeVisible();
+    });
 });
