cf8f56197e14131fc168a1ba78c995306b1dacd9
Henil <henilp467@gmail.com>
2025-10-09 13:41:57 +1100

    [e2e/contactForm.spec.js]: conduct e2e test for success message upon valid contact form submission

--- tests/e2e/contactForm.spec.js
+++ tests/e2e/contactForm.spec.js
@@ -15,4 +15,19 @@ test.describe("End-to-End tests for Contact Form", () => {
         await page.click(".submit-btn");
         await expect(page.locator(".error-message")).toBeVisible();
     });
+
+    // Test to show success message on successful contact form submission
+    test("submits form successfully and shows success message", async ({ page }) => {
+        await page.goto("http://localhost:5173/contact");
+
+        await page.fill("#firstName", "John");
+        await page.fill("#lastName", "Doe");
+        await page.fill("#email", "john@example.com");
+        await page.fill("#message", "This is a Playwright test message.");
+        await page.click(".submit-btn");
+
+        await expect(page.locator(".success-message")).toHaveText(
+        "Thank you for contacting us! We’ll get back to you soon."
+        );
+    });
 });
