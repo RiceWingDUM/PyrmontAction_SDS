d773be7984d9a1e2144c5148efad717a68d2f1e8
Henil <henilp467@gmail.com>
2025-10-09 20:41:47 +1100

    [e2e/home.spec.js]: UI test to verify if Carousel slide dots are visible on home page

--- tests/e2e/home.spec.js
+++ tests/e2e/home.spec.js
@@ -6,4 +6,12 @@ test.describe("Carousel UI Behavior", () => {
     test.beforeEach(async ({ page }) => {
         await page.goto("http://localhost:5173/"); // your local dev URL
     });
+
+    // Verify if dots and 1st slide are visible on homepage
+    test("should display pagination dots and first slide active by default", async ({ page }) => {
+        const activeDot = page.locator(".pagination .active");
+
+        // Assert that the first dot is active
+        await expect(activeDot).toBeVisible();
+    });
 });
