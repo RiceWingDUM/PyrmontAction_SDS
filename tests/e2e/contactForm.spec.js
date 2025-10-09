fe670cde401f2bf13376e5c84d0adc653f0532a3
Henil <henilp467@gmail.com>
2025-10-09 13:25:28 +1100

    [e2e/contactForm.spec.js]: implement UI test to check if contact form fields render correctly

--- tests/e2e/contactForm.spec.js
+++ tests/e2e/contactForm.spec.js
@@ -2,5 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test.describe("End-to-End tests for Contact Form", () => {
 
-
+    // Check if contact form fields render correctly
+    test("renders all fields correctly", async ({ page }) => {
+        await page.goto("http://localhost:5173/contact");
+        await expect(page.locator("#firstName")).toBeVisible();
+        await expect(page.locator("#email")).toBeVisible();
+    });
 });
